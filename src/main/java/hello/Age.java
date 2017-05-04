ee5ef48493133445ad65e3913a2fa47bcf38103c
masuda220 <masuda220@gmail.com>
2017-05-04 10:17:06 +0900

    getter/setter

--- src/main/java/hello/Age.java
+++ src/main/java/hello/Age.java
@@ -17,7 +17,7 @@ public class Age {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
 }
