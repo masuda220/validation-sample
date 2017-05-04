b98ecb38cbd8143aced5bd8d051753925fa30478
masuda220 <masuda220@gmail.com>
2017-05-04 14:21:06 +0900

    no getter/setter

--- src/main/java/hello/Age.java
+++ src/main/java/hello/Age.java
@@ -1,14 +1,14 @@
 package hello;
 
-import org.hibernate.validator.constraints.NotEmpty;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 
 public class Age {
 
-    @NotEmpty(message = "必須")
+    @NotNull(message = "必須")
     @Min(value=18,message = "１８歳以上")
-    Integer age;
+    int age;
 
     public Age(String age) {
         this.age = Integer.valueOf(age);
