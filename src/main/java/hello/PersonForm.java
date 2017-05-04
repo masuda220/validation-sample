518e02f7807206853c0d730467b107b57af011c0
masuda220 <masuda220@gmail.com>
2017-05-04 10:25:39 +0900

    getter/setter

--- src/main/java/hello/PersonForm.java
+++ src/main/java/hello/PersonForm.java
@@ -8,11 +8,12 @@ import javax.validation.constraints.Size;
 public class PersonForm {
 
     @NotNull
-    @Size(min=2, max=30)
+    @Size(min=2, max=30, message = "{min}以上 {max}以下")
     private String name;
 
-    @Valid
-    private Age age;
+    @NotNull
+    @Min(value=18,message = "１８歳以上")
+    int age;
 
     public String getName() {
         return name;
@@ -22,13 +23,21 @@ public class PersonForm {
         this.name = name;
     }
 
-    public Age getAge() {
+    public int getAge() {
         return age;
     }
 
-    public void setAge(Age age) {
+    public void setAge(int age) {
         this.age = age;
     }
+
+    //    public Age getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(Age age) {
+//        this.age = age;
+//    }
 
     public String toString() {
         return "Person(Name: " + this.name + ", Age: " + this.age + ")";
