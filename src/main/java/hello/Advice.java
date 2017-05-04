7649937c462ec02c402a49edef202633d39279c5
masuda220 <masuda220@gmail.com>
2017-05-04 10:08:59 +0900

    getter/setter

--- src/main/java/hello/Advice.java
+++ src/main/java/hello/Advice.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.InitBinder;
 
-@ControllerAdvice
+//@ControllerAdvice
 public class Advice {
 
-    @InitBinder
+    //@InitBinder
     public void initBinder(WebDataBinder binder) {
         binder.initDirectFieldAccess();
 
--- src/main/java/hello/PersonForm.java
+++ src/main/java/hello/PersonForm.java
@@ -14,6 +14,22 @@ public class PersonForm {
     @Valid
     private Age age;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Age getAge() {
+        return age;
+    }
+
+    public void setAge(Age age) {
+        this.age = age;
+    }
+
     public String toString() {
         return "Person(Name: " + this.name + ", Age: " + this.age + ")";
     }
