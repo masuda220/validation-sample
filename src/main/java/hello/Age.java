5544547119585557c6884a28ae07ae2fd994be2b
masuda220 <masuda220@gmail.com>
2017-05-04 11:59:22 +0900

    no getter/setter

--- src/main/java/hello/Age.java
+++ src/main/java/hello/Age.java
@@ -9,7 +9,7 @@ public class Age {
     @Min(value=18,message = "１８歳以上")
     Integer age;
 
-//    public Age(String age) {
-//        this.age = Integer.valueOf(age);
-//    }
+    public Age(String age) {
+        this.age = Integer.valueOf(age);
+    }
 }
