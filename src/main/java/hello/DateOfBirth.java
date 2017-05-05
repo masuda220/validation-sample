e126fa5c3c11914393cd9b1017e67eaa30ee321a
masuda220 <masuda220@gmail.com>
2017-05-05 15:53:07 +0900

    誕生日を追加

--- src/main/java/hello/DateOfBirth.java
+++ src/main/java/hello/DateOfBirth.java
@@ -1,10 +1,13 @@
 package hello;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public class DateOfBirth {
 
 	@NotNull(message="必須")
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
 	LocalDate date ;
 }
